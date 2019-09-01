90f1eb462c1f705383208e7c00ffc232a99c22e4
OlegNG86 <51402092+OlegNG86@users.noreply.github.com>
2019-09-01 21:19:22 +0300

    Solved 06_songs_list

--- python_homeworks/lesson_002/06_songs_list.py
+++ python_homeworks/lesson_002/06_songs_list.py
@@ -20,7 +20,11 @@ violator_songs_list = [
 # Обратите внимание, что делать много вычислений внутри print() - плохой стиль.
 # Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
 
-# TODO здесь ваш код
+halo_lenght = violator_songs_list[3][1]
+enjoy_lenght = violator_songs_list[5][1]
+clean_lenght = violator_songs_list[-1][1]
+sum = halo_lenght + enjoy_lenght + clean_lenght
+print('Три песни звучат ' + str(round(sum, 2)) + ' минут')
 
 # Есть словарь песен группы Depeche Mode
 violator_songs_dict = {
@@ -38,4 +42,8 @@ violator_songs_dict = {
 # распечатайте общее время звучания трех песен: 'Sweetest Perfection', 'Policy of Truth' и 'Blue Dress'
 #   А другие три песни звучат ХХХ минут
 
-# TODO здесь ваш код
+sweet = violator_songs_dict['Sweetest Perfection']
+policy = violator_songs_dict['Policy of Truth']
+blue = violator_songs_dict['Blue Dress']
+sum_2 = sweet + policy + blue
+print('А другие три песни звучат ' + str(round(sum_2, 2)) + ' минут')
