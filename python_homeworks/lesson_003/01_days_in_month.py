5f91043fac892e08da19e5472b6f86faa2858899
Oleg Pykhalov <pykhalov.oleg@gmail.com>
2019-09-03 09:29:11 +0300

    Solved 01_days_in_month

--- python_homeworks/lesson_003/01_days_in_month.py
+++ python_homeworks/lesson_003/01_days_in_month.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import calendar
 
 # (if/elif/else)
 
@@ -7,8 +8,14 @@
 # Если номер месяца некорректен - сообщить об этом
 
 # Номер месяца получать от пользователя следующим образом
+
 user_input = input("Введите, пожалуйста, номер месяца: ")
 month = int(user_input)
-print('Вы ввели', month)
 
-# TODO здесь ваш код
+if 0 < month < 13:
+    print('Вы ввели', month)
+    monthrange = calendar.monthrange(2019, month)
+    number_of_day = monthrange[1]
+    print(number_of_day)
+else:
+    print('Месяцев всего 12, вы ввели неверное значение!')
