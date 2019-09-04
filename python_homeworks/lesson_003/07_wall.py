85f5d0dd2b40ef3996dafd920abbcb60a171d5c2
Oleg Pykhalov <pykhalov.oleg@gmail.com>
2019-09-04 17:54:32 +0300

    UnSolved 07_wall

--- python_homeworks/lesson_003/07_wall.py
+++ python_homeworks/lesson_003/07_wall.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import simple_draw
+import simple_draw as sd
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
+point_left_bottom = sd.get_point(100, 100)
+point_right_top = sd.get_point(200, 150)
 
-# TODO здесь ваш код
 
-simple_draw.pause()
+sd.rectangle(left_bottom=point_left_bottom, right_top=point_right_top)
+
+
+sd.pause()
