4d8898f96ff93b50f8ccf99b15814324572f4913
Oleg Pykhalov <pykhalov.oleg@gmail.com>
2019-09-05 09:21:19 +0300

    Solved 07_wall

--- python_homeworks/lesson_003/07_wall.py
+++ python_homeworks/lesson_003/07_wall.py
@@ -5,11 +5,19 @@ import simple_draw as sd
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
-point_left_bottom = sd.get_point(100, 100)
-point_right_top = sd.get_point(200, 150)
+for i in range(50, 400, 55):
+    X_left = 100
+    Y_left = 100+i
+    X_right = 200
+    Y_right = 150+i
+    point_left_bottom = sd.get_point(X_left, Y_left)
+    point_right_top = sd.get_point(X_right, Y_right)
+    sd.rectangle(left_bottom=point_left_bottom, right_top=point_right_top)
+    for j in range(100, 500, 105):
+        point_left_bottom = sd.get_point(X_left+5+j, Y_left)
+        point_right_top = sd.get_point(X_right+5+j, Y_right)
+        sd.rectangle(left_bottom=point_left_bottom, right_top=point_right_top)
 
-
-sd.rectangle(left_bottom=point_left_bottom, right_top=point_right_top)
 
 
 sd.pause()
