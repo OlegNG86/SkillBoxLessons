21a4650170b404224e4de3b6e593fec0f735cc6c
OlegNG86 <51402092+OlegNG86@users.noreply.github.com>
2019-09-13 22:55:21 +0300

    Solved 01_inhabitants

--- python_homeworks/lesson_005/01_inhabitants.py
+++ python_homeworks/lesson_005/01_inhabitants.py
@@ -3,6 +3,9 @@
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-# TODO здесь ваш код
+from room_1 import folks
+from room_2 import folks as fs
+print('В комнате room_1 живут: ', folks)
+print('В комнате room_2 живёт: ', fs)
 
 
