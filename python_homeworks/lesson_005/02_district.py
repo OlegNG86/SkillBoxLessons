73aa7338d218d0f3d322b6294c7a4521a717fe65
OlegNG86 <51402092+OlegNG86@users.noreply.github.com>
2019-09-13 23:33:36 +0300

    Solved 02_district

--- python_homeworks/lesson_005/02_district.py
+++ python_homeworks/lesson_005/02_district.py
@@ -5,8 +5,19 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
+
+from district.central_street.house1 import room1 as cent1room, room2 as cent2room
+from district.central_street.house2 import room1 as r1, room2 as r2
+
+from district.soviet_street.house1 import room1 as sh1r1, room2 as sh1r2
+from district.soviet_street.house2 import room1 as sh2r1, room2 as sh2r2
+
+# from district.soviet_street.house1 import room1.folks as rf
+
+print('На Центральной улице живут: ')
+
+print(', '.join(cent1room.folks + cent2room.folks + r1.folks + r2.folks))
 
 
-
-
+print('\nНа Советской улице живут: ')
+print(', '.join(sh1r1.folks + sh1r2.folks + sh2r1.folks + sh2r2.folks))
