d9ba30d7a586ed1da4ab9932e2c7742d0ae8f7ea
Oleg Pykhalov <pykhalov.oleg@gmail.com>
2019-09-27 08:17:48 +0300

    Solved 01_family (добавил инит жене).

--- python_homeworks/lesson_008/01_family.py
+++ python_homeworks/lesson_008/01_family.py
@@ -75,8 +75,10 @@ class Husband:
 
 class Wife:
 
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        self.name = name
+        self.fullness = 30
+        self.happiness = 100
 
     def __str__(self):
         return super().__str__()
