8ea928842ad5c75e7e54499370a679da598926f3
OlegNG86 <51402092+OlegNG86@users.noreply.github.com>
2019-10-04 22:47:21 +0300

    It's working!

--- python_homeworks/lesson_009/01_char_stat.py
+++ python_homeworks/lesson_009/01_char_stat.py
@@ -20,8 +20,38 @@
 #
 # Упорядочивание по частоте - по убыванию. Ширину таблицы подберите по своему вкусу
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
+import os
+from pprint import pprint
 
-# TODO здесь ваш код
+
+folder = os.getcwd()
+# file_name = 'voyna-i-mir.txt.zip'
+file_name = 'pushkin.txt'
+
+pushkin_folder = 'python_snippets'
+fullpath = os.path.join(folder, pushkin_folder, file_name)
+
+with open(fullpath, 'r', encoding='utf8') as opened_file:
+    text_from_pushkin = opened_file.read()
+    print(text_from_pushkin)
+
+sum_all_char = 0
+char_dict = {}
+
+for i in range(1040, 1104):
+    char = chr(i)
+    char_count = text_from_pushkin.count(char)
+    sum_all_char += char_count
+    char_dict[char] = char_count
+print(char_dict)
+
+
+
+
+
+
+
+
 
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
