8f53309612ebd5c7b19a7207a53a5fe33c0d3a2e
OlegNG86 <pykhalov.oleg@gmail.com>
2019-10-12 15:18:28 +0300

    Solved_01_fifth_element

--- python_homeworks/lesson_010/01_fifth_element.py
+++ python_homeworks/lesson_010/01_fifth_element.py
@@ -4,10 +4,16 @@
 
 BRUCE_WILLIS = 42
 
-input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
-leeloo = int(input_data[4])
-result = BRUCE_WILLIS * leeloo
-print(f"- Leeloo Dallas! Multi-pass № {result}!")
+
+try:
+    input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
+    leeloo = int(input_data[4])
+    result = BRUCE_WILLIS * leeloo
+    print(f"- Leeloo Dallas! Multi-pass № {result}!")
+except ValueError as exc:
+    print(f'Невозможно преобразовать к числу, {exc}')
+except IndexError as exc:
+    print(f'Выход за границы списка, {exc}')
 
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
