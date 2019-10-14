50927e963031dc773798c9f77e5c79054b6070b6
Oleg Pykhalov <pykhalov.oleg@gmail.com>
2019-10-14 08:28:53 +0300

    Solved_03_registration

--- python_homeworks/lesson_010/03_registration_log.py
+++ python_homeworks/lesson_010/03_registration_log.py
@@ -45,7 +45,8 @@ class FileChecker:
         self.user_name_check = r'\b[А-Я][А-я]*'
         self.user_email = None
         self.user_email_check = r'\b[a-z]*[.]*[a-z]*@\w*\.\w*'
-        self.pattern = re.compile(self.user_name_check+str([ ])+self.user_email_check+str([ ])+self.user_age_check)
+        self.pattern = re.compile(
+            self.user_name_check + str([]) + self.user_email_check + str([]) + self.user_age_check)
 
     def __str__(self):
         return f'Запущен файл {self.name}'
@@ -62,42 +63,38 @@ class FileChecker:
             f.writelines(self.data)
 
     def check_file(self):
-        # bad_list = []
-            count = 0
-            for i in self.data:
-                try:
+        good_list = []
+        bad_list = []
+        count = 0
+        for i in self.data:
+            try:
+                if len(re.split(' ', i)) == 3:
+                    self.user_name, self.user_email, self.user_age = re.split(' ', i)
+                    self.user_name = re.findall(self.user_name_check, self.user_name)
+                    self.user_age = re.findall(self.user_age_check, self.user_age)
+                    self.user_email = re.findall(self.user_email_check, self.user_email)
+                    if len(self.user_name) + len(self.user_email) + len(self.user_age) == 3:
+                        concatenate = self.user_name[0] + ' ' + self.user_email[0] + ' ' + self.user_age[0] + '\n'
+                        good_list.append(concatenate)
+                    count += 1
 
-                    if len(re.split(' ', i)) == 3:
-                        self.user_name, self.user_email, self.user_age = re.split(' ', i)
-                        # print(self.user_email)
-                        self.user_name = re.findall(self.user_name_check, self.user_name)
-                        self.user_age = re.findall(self.user_age_check, self.user_age)
-                        self.user_email = re.findall(self.user_email_check, self.user_email)
-                        print(len(self.user_name), len(self.user_email), len(self.user_age))
-                        count += 1
-                        # self.user_name, self.user_email, self.user_age = i.split(sep=' ')
-                        # print(self.user_name if self.user_name_check.search(self.user_name) != None)
-                        # if len(i.split(sep=' ')) != 3:
-                        #     raise ValueError('Не присутствуют все три поля')
+                    if len(self.user_name) < 1:
+                        raise NotNameError('Поле имени содержит НЕ только буквы.')
+                    if len(self.user_email) < 1:
+                        raise NotEmailError('Адрес почты указан некорректно.')
+                    if len(self.user_age) < 1:
+                        raise ValueError('Поле возраст НЕ является числом от 10 до 99.')
+                else:
+                    raise ValueError('НЕ присутсвуют все три поля.')
 
-                            # if not self.user_name_check == False:
-                            #     raise NotNameError('В имени не только буквы.')
-                            # if not self.user_email_check == False:
-                            #     raise NotEmailError('Адрес почты указан некорректно.')
-                            # if not self.user_age_check == False:
-                            #     raise ValueError('Возраст указан некорректно.')
-                        # else:
-                        #     bad_list.append(i)
+            except Exception as exc:
+                print(f'Была ошибка. {exc}')
+                bad_list.append(f'{i} {exc}')
 
-
-                except Exception as exc:
-                    print(f'Была ошибка {exc}')
-
-                finally:
-                    pass
-                    # self.write_new_file('registrations_bad.log', bad_list)
-                    print(count)
-
+            finally:
+                self.write_new_file('registrations_bad.log', bad_list)
+                print(count)
+                self.write_new_file('registrations_good.log', good_list)
 
     def print_strings(self):
         count = 0
