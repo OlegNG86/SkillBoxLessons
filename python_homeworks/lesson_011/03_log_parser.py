03895cdc31c6a66556e9d5b6314fade3f9f97c31
OlegNG86 <51402092+OlegNG86@users.noreply.github.com>
2019-10-22 20:30:12 +0300

    Solved 09_shopping

--- python_homeworks/lesson_011/03_log_parser.py
+++ python_homeworks/lesson_011/03_log_parser.py
@@ -14,4 +14,21 @@
 #
 # [2018-05-17 01:57] 1234
 
-# TODO здесь ваш код
+def generator(file, group_by):
+    counter = 0
+    with open(file, 'r', encoding='utf-8') as f:
+        for i in f:
+            if group_by in i:
+                counter += 1
+                a, b, c = i.split()
+                yield a[1:] + ' ' + b[:5], counter
+
+
+filename = 'events.txt'
+group_by = 'NOK'
+
+gen = generator(filename, group_by)
+
+grouped_events = generator(filename, group_by)
+for group_time, event_count in grouped_events:
+    print(f'[{group_time}] {event_count}')
